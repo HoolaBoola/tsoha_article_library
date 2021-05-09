9c1d79eb06811a97c6984d4c970ee71a18724df7
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2021-05-10 00:45:03 +0300

    Removed most unnecessary lines from schema.sql

--- schema.sql
+++ schema.sql
@@ -1,38 +1,8 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 13.2
--- Dumped by pg_dump version 13.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: article_group; Type: TABLE; Schema: public; Owner: -
---
-
 CREATE TABLE public.article_group (
     article_id integer,
     group_id integer
 );
 
-
---
--- Name: articles; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.articles (
     id integer NOT NULL,
@@ -47,10 +17,6 @@ CREATE TABLE public.articles (
 );
 
 
---
--- Name: article_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
 CREATE SEQUENCE public.article_id_seq
     AS integer
     START WITH 1
@@ -60,16 +26,8 @@ CREATE SEQUENCE public.article_id_seq
     CACHE 1;
 
 
---
--- Name: article_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.article_id_seq OWNED BY public.articles.id;
 
-
---
--- Name: article_topic; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.article_topic (
     article_id integer,
@@ -77,20 +35,12 @@ CREATE TABLE public.article_topic (
 );
 
 
---
--- Name: comments; Type: TABLE; Schema: public; Owner: -
---
-
 CREATE TABLE public.comments (
     id integer NOT NULL,
     article_id integer,
     name character varying(255)
 );
 
-
---
--- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE public.comments_id_seq
     AS integer
@@ -101,16 +51,8 @@ CREATE SEQUENCE public.comments_id_seq
     CACHE 1;
 
 
---
--- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
 
-
---
--- Name: groups; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.groups (
     id integer NOT NULL,
@@ -118,10 +60,6 @@ CREATE TABLE public.groups (
     description text
 );
 
-
---
--- Name: groups_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE public.groups_id_seq
     AS integer
@@ -132,26 +70,14 @@ CREATE SEQUENCE public.groups_id_seq
     CACHE 1;
 
 
---
--- Name: groups_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.groups_id_seq OWNED BY public.groups.id;
 
-
---
--- Name: interest_groups; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.interest_groups (
     id integer NOT NULL,
     name character varying(255) NOT NULL
 );
 
-
---
--- Name: interest_group_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE public.interest_group_id_seq
     AS integer
@@ -162,26 +88,14 @@ CREATE SEQUENCE public.interest_group_id_seq
     CACHE 1;
 
 
---
--- Name: interest_group_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.interest_group_id_seq OWNED BY public.interest_groups.id;
 
-
---
--- Name: topics; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.topics (
     id integer NOT NULL,
     name character varying(255) NOT NULL
 );
 
-
---
--- Name: topic_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE public.topic_id_seq
     AS integer
@@ -192,16 +106,8 @@ CREATE SEQUENCE public.topic_id_seq
     CACHE 1;
 
 
---
--- Name: topic_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.topic_id_seq OWNED BY public.topics.id;
 
-
---
--- Name: user_group; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.user_group (
     user_id integer,
@@ -209,19 +115,11 @@ CREATE TABLE public.user_group (
 );
 
 
---
--- Name: user_topic; Type: TABLE; Schema: public; Owner: -
---
-
 CREATE TABLE public.user_topic (
     user_id integer,
     topic_id integer
 );
 
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: -
---
 
 CREATE TABLE public.users (
     id integer NOT NULL,
@@ -229,10 +127,6 @@ CREATE TABLE public.users (
     password character varying(255) NOT NULL
 );
 
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE public.users_id_seq
     AS integer
@@ -243,192 +137,91 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
-
---
--- Name: articles id; Type: DEFAULT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.articles ALTER COLUMN id SET DEFAULT nextval('public.article_id_seq'::regclass);
 
 
---
--- Name: comments id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
 
-
---
--- Name: groups id; Type: DEFAULT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.groups ALTER COLUMN id SET DEFAULT nextval('public.groups_id_seq'::regclass);
 
 
---
--- Name: interest_groups id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.interest_groups ALTER COLUMN id SET DEFAULT nextval('public.interest_group_id_seq'::regclass);
 
-
---
--- Name: topics id; Type: DEFAULT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.topics ALTER COLUMN id SET DEFAULT nextval('public.topic_id_seq'::regclass);
 
 
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
-
---
--- Name: articles article_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.articles
     ADD CONSTRAINT article_pkey PRIMARY KEY (id);
 
 
---
--- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.comments
     ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
 
-
---
--- Name: groups groups_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.groups
     ADD CONSTRAINT groups_pkey PRIMARY KEY (id);
 
 
---
--- Name: interest_groups interest_group_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.interest_groups
     ADD CONSTRAINT interest_group_pkey PRIMARY KEY (id);
 
-
---
--- Name: topics topic_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.topics
     ADD CONSTRAINT topic_pkey PRIMARY KEY (id);
 
 
---
--- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT unique_username UNIQUE (username);
 
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
---
--- Name: article_group article_group_article_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.article_group
     ADD CONSTRAINT article_group_article_id_fkey FOREIGN KEY (article_id) REFERENCES public.articles(id);
 
-
---
--- Name: article_group article_group_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.article_group
     ADD CONSTRAINT article_group_group_id_fkey FOREIGN KEY (group_id) REFERENCES public.groups(id);
 
 
---
--- Name: article_topic article_topic_article_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.article_topic
     ADD CONSTRAINT article_topic_article_id_fkey FOREIGN KEY (article_id) REFERENCES public.articles(id);
 
-
---
--- Name: article_topic article_topic_topic_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.article_topic
     ADD CONSTRAINT article_topic_topic_id_fkey FOREIGN KEY (topic_id) REFERENCES public.topics(id);
 
 
---
--- Name: articles articles_creator_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.articles
     ADD CONSTRAINT articles_creator_fkey FOREIGN KEY (creator) REFERENCES public.users(id);
 
-
---
--- Name: comments fk_article; Type: FK CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.comments
     ADD CONSTRAINT fk_article FOREIGN KEY (article_id) REFERENCES public.articles(id);
 
 
---
--- Name: user_group user_group_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.user_group
     ADD CONSTRAINT user_group_group_id_fkey FOREIGN KEY (group_id) REFERENCES public.groups(id);
 
-
---
--- Name: user_group user_group_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
 
 ALTER TABLE ONLY public.user_group
     ADD CONSTRAINT user_group_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
---
--- Name: user_topic user_topic_topic_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.user_topic
     ADD CONSTRAINT user_topic_topic_id_fkey FOREIGN KEY (topic_id) REFERENCES public.topics(id);
 
 
---
--- Name: user_topic user_topic_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.user_topic
     ADD CONSTRAINT user_topic_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- PostgreSQL database dump complete
---
 
